d15bb759f2b422e829f27735208e6ba5013dffa3
paulni <paul86677889@gmail.com>
2023-04-17 12:10:01 +0800

    fix type srcipt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "/styles/globals.css";
 import { WrapContext } from "../src/store/index";
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }:any) {
   return (
     <WrapContext>
       <Component {...pageProps} />
