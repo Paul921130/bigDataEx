b85dab646cb21a8928869450e48da57191d55021
paulni <paul86677889@gmail.com>
2023-04-17 14:49:05 +0800

    add bk component

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./background.module.css";
 
 const Background = () => {
-    return <></>;
+    return (
+    <div>
+        <h1>
+            TAIWAN HAHA
+        </h1>
+    </div>);
 };
 
 export default Background;
